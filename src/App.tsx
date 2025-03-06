0a0c6769c42ccfb4b505fba69bba3fe45ef095d1
Ariel-250225 <Ariel-250225@users.noreply.github.com>
2025-03-06 14:24:12 +0000

    Prettified Code!

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  const googleAPISecretsKey = "sadfaksdmiiidfngkermasdfd"
+  const googleAPISecretsKey = "sadfaksdmiiidfngkermasdfd";
 
   return (
     <>
