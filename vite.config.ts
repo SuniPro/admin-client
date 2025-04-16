4c6999c1414c104fbe9efee90cc5e854dbe29f83
Ariel-250225 <seobin250225@icloud.com>
2025-04-16 22:03:04 +0700

    feature : 프로젝트 구성파일 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,32 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+  ],
+  server: {
+    host: true,
+    port: 5130,
+    proxy: {
+      // 첫 번째 프록시 설정
+      "/admin": {
+        target: "http://localhost:8010",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/admin/, ""),
+        secure: false,
+        ws: true,
+      },
+      "/user": {
+        target: "http://13.214.147.245:8070",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/user/, ""),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 });
