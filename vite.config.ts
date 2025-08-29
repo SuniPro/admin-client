945622421f89a08e31753e2f8eacb01ff049a2af
SuniPro <thtnsgus21@gmail.com>
2025-08-30 02:57:35 +0700

    style : tailwind, change proxy prefix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,9 +15,11 @@ export default defineConfig({
         plugins: ["@emotion/babel-plugin"],
       },
     }),
+    tsconfigPaths(),
   ],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "src"),
       "@modules/lexical-editor/lexical": path.resolve(
         __dirname,
         "src/modules/lexical-editor/lexical",
@@ -36,10 +39,17 @@ export default defineConfig({
         secure: false,
         ws: true,
       },
-      "/user": {
+      "/forward": {
         target: "http://13.212.255.35:8020",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/user/, ""),
+        rewrite: (path) => path.replace(/^\/forward/, ""),
+        secure: false,
+        ws: true,
+      },
+      "/tracker": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/tracker/, ""),
         secure: false,
         ws: true,
       },
