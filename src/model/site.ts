bfcd757f460a88c1b7b573ae8003172dc351b526
SuniPro <thtnsgus21@gmail.com>
2025-09-03 19:51:43 +0700

    refactor : site 외래키 추가에 따라 api endpoint 등 일부 변경

--- src/model/site.ts
+++ src/model/site.ts
@@ -1,14 +1,46 @@
 import { ChainType } from "./financial";
 
-export interface SiteResponseType {
+export interface SiteType {
   id: number;
   site: string;
-  cryptoWallet: string;
-  chainType: ChainType;
+  siteWalletList: SiteWalletType[];
+  telegramUsername: string;
+  telegramChatId: number;
   insertDateTime: string;
   insertId: string;
   updateDateTime: string | null;
   updateId: string | null;
   deleteDateTime: string | null;
   deleteId: string | null;
+}
+
+export interface SiteOnlyType {
+  id: number;
+  site: string;
+  telegramUsername: string;
+  telegramChatId: number;
+  insertDateTime: string;
+  insertId: string;
+  updateDateTime: string | null;
+  updateId: string | null;
+  deleteDateTime: string | null;
+  deleteId: string | null;
+}
+
+export interface SiteWalletType {
+  id: number;
+  cryptoWallet: string;
+  chainType: ChainType;
+}
+
+export interface SiteWalletInfoType {
+  id: number;
+  cryptoWallet: string;
+  chainType: ChainType;
+  balance: string;
+  depositHistoryLength: number;
+  todayDepositAmount: string;
+  weeksDepositAmount: string;
+  insertDateTime: string;
+  updateDateTime: string;
 }
